9437cf4c3ee2377fedcea088e0dedd86280a7734
chirag <ch.irax000@gmail.com>
2026-05-26 19:35:08 +0530

    cleaned folder

---
+++ 01_vite_project/src/App.jsx
@@ -0,0 +1,15 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <h1>MAVERICK</h1>
+  )
+}
+
+export default App
