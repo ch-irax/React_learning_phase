3fe8aba97a22b365f7f4312d057402a0db20ac35
chirag <ch.irax000@gmail.com>
2026-05-27 13:42:47 +0530

    new component

--- 01_vite_project/src/App.jsx
+++ 01_vite_project/src/App.jsx
@@ -2,13 +2,14 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import Tryout from './try'
 
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <h1>MAVERICK</h1>
+    <>
+      <Tryout />
+    </>
   )
 }
 
---
+++ 01_vite_project/src/try.jsx
@@ -0,0 +1,7 @@
+function Tryout() {
+    return(
+        <h2>Trying new function</h2>
+        )
+}
+
+export default Tryout
